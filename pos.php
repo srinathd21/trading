61cffdfd335b50ee8ff1c22a5817333a9bcdc1ac
ecommerofficial <ecommerofficial@gmail.com>
2026-03-28 12:58:00 +0530

    updated

--- pos.php
+++ pos.php
@@ -5,6 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>POS - Billing</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="assets/favicon/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="assets/favicon//favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="assets/favicon//favicon-16x16.png">
+<link rel="manifest" href="assets/favicon//site.webmanifest">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
